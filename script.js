6ff2ff3656d7f43df65526be59b1462afbcd626d
rafaelst2000 <rafaelst2000@gmail.com>
2023-01-12 14:38:56 -0300

    ✨ finish blackjack game

--- script.js
+++ script.js
@@ -1,27 +1,171 @@
-let allCards = []
+function Player(name, count, cards) {
+  this.name = name;
+  this.count = count;
+  this.cards = cards;
+}
 
-fetch('https://deckofcardsapi.com/api/deck/new/')
+const name1 = prompt('Digite o nome do Jogador 1')
+const name2 = prompt('Digite o nome do Jogador 2')
+const name3 = prompt('Digite o nome do Jogador 3')
+
+let deck = []
+let playerTurn = 1
+let isGameFinished = false
+
+const deckImgBtn = document.querySelector('.deck-img')
+const stopBtn = document.querySelector('.stop-button')
+const newGameBtn = document.querySelector('.new-game-button')
+let player1 = new Player(name1, 0, [])
+let player2 = new Player(name2, 0, [])
+let player3 = new Player(name3, 0, [])
+
+drawPlayerName(player1.name)
+drawPlayersScore()
+
+fetch('https://deckofcardsapi.com/api/deck/new/shuffle/')
   .then(response => response.json())
   .then(result => {
     const { deck_id } = result
     fetch(`https://deckofcardsapi.com/api/deck/${deck_id}/draw/?count=52`)
       .then(response => response.json())
       .then(result => {
-        allCards = result.cards
-        drawCards()
+        deck = result.cards
       })
   })
 
+function drawPlayerName(name) {
+  const container = document.querySelector('h3')
+  container.textContent = `Vez de: ${name}`
+}
 
-function drawCards() {
-  const container = document.querySelector('.player-1-cards')
-  const cards = [allCards[0], allCards[1], allCards[2], allCards[3], allCards[3] ,allCards[3]]
-  for(let i = 0; i < cards.length; i++) {
-    const newCardImg = document.createElement('img')
-    newCardImg.src = cards[i].images.png
-    newCardImg.classList.add('absolute-card')
-    newCardImg.style.left = `${i*40}px`
+function makePlay(player) {
+  if(player.count > 21 || isGameFinished) return
+  const newCard = deck[0]
 
-    container.appendChild(newCardImg)
+  player.cards.push(newCard);
+  deck.shift()
+
+  drawCards(player, newCard)
+  player.count += getCardValue(newCard)
+  showPlayerScoreAtScreen(player.count)
+
+  if(player.count == 21) {
+    winGame(player)
+  }
+
+  if(player.count > 21) { 
+    handleClickStopOrExplode()
+    alert('Estourou!')
   }
 }
+
+function drawCards(player, newCard) {
+  const container = document.querySelector('.cards')
+  const length = player.cards.length
+  const newCardImg = document.createElement('img')
+  
+  newCardImg.src = player.cards[length - 1].images.png
+  newCardImg.classList.add('absolute-card')
+  container.appendChild(newCardImg)
+}
+
+function getCardValue(card) {
+  const { value } = card
+  if(['ACE', 'JACK', 'QUEEN', 'KING'].includes(value)) {
+    const cards = {
+      ACE: 1,
+      JACK: 11,
+      QUEEN: 12,
+      KING: 13
+    }
+    return cards[value]
+  }
+  return Number(value)
+}
+
+function showPlayerScoreAtScreen(score) {
+  const container = document.querySelector('.total-score')
+  container.textContent = `Total: ${score}`
+}
+
+function handleClickStopOrExplode() {
+  playerTurn++
+  drawPlayersScore()
+  clearScreen()
+  if(playerTurn < 4) {
+    const players = { 1: player1.name, 2: player2.name, 3: player3.name }
+    drawPlayerName(players[playerTurn])
+  } else {
+    verifyScore()
+  }
+}
+
+function clearScreen() {
+  const score = document.querySelector('.total-score')
+  score.textContent = `Total: 0`
+
+  const cards = document.querySelector('.cards')
+  while (cards.firstChild) {
+    cards.removeChild(cards.firstChild)
+  }
+}
+
+function drawPlayersScore() {
+  const p1 = document.querySelector('.player-1-score')
+  const p2 = document.querySelector('.player-2-score')
+  const p3 = document.querySelector('.player-3-score')
+
+  p1.textContent = `${player1.name}: ${player1.count}`
+  p2.textContent = `${player2.name}: ${player2.count}`
+  p3.textContent = `${player3.name}: ${player3.count}`
+}
+
+function verifyScore() {
+  const container = document.querySelector('h3')
+  const players = [player1, player2, player3]
+  const orderedValidScorePlayers = players.filter(player => player.count < 22).sort((a,b) => b.count - a.count)
+
+  if(!orderedValidScorePlayers.length) container.textContent = 'Nenhum jogador venceu :('
+  else {
+    if(orderedValidScorePlayers.length === 3 && orderedValidScorePlayers[0].count === orderedValidScorePlayers[1].count && orderedValidScorePlayers[0].count === orderedValidScorePlayers[2].count) container.textContent = 'Empate dos 3!'
+    else if(orderedValidScorePlayers.length === 2 && (orderedValidScorePlayers[0].count === orderedValidScorePlayers[1].count)) container.textContent = `Empate de ${orderedValidScorePlayers[0].name} e ${orderedValidScorePlayers[1].name}!`
+    else container.textContent = `Vitória de ${orderedValidScorePlayers[0].name}!!!`
+  }
+
+  isGameFinished = true
+
+  const stopBtn = document.querySelector('.stop-button')
+  stopBtn.classList.toggle('hidden')
+
+  const newGame = document.querySelector('.new-game-button')
+  newGame.classList.toggle('hidden')
+}
+
+function winGame(player) {
+  isGameFinished = true
+  const container = document.querySelector('h3')
+  container.textContent = `Vitória de ${player.name}!!!`
+  stopBtn.classList.toggle('hidden')
+  newGameBtn.classList.toggle('hidden')
+}
+
+function newGame() {
+  isGameFinished = false
+  player1 = new Player(name1, 0, [])
+  player2 = new Player(name2, 0, [])
+  player3 = new Player(name3, 0, [])
+  playerTurn = 0
+
+  handleClickStopOrExplode()
+  stopBtn.classList.toggle('hidden')
+  newGameBtn.classList.toggle('hidden')
+}
+
+deckImgBtn.addEventListener('click', () => {
+  if(playerTurn === 1) makePlay(player1)
+  if(playerTurn === 2) makePlay(player2)
+  if(playerTurn === 3) makePlay(player3)
+})
+
+stopBtn.addEventListener('click', handleClickStopOrExplode)
+newGameBtn.addEventListener('click', newGame)
